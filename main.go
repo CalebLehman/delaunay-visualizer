fa99589210ce2ad7108fc5110d5ef3422ccd2e73
CalebLehman <lehman.346@osu.edu>
2020-03-20 22:13:49 -0400

    Re-format Go code with gofmt

--- main.go
+++ main.go
@@ -1,38 +1,43 @@
 package main
 
 import (
-    "net/http"
-    "fmt"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func uploadVertices(w http.ResponseWriter, r *http.Request) {
-    var triangulation Triangulation
-    err := json.NewDecoder(r.Body).Decode(&triangulation)
-    if err != nil {
-        fmt.Println("Error decoding JSON")
-        fmt.Println(err)
-        return
-    }
+	var triangulation Triangulation
+	err := json.NewDecoder(r.Body).Decode(&triangulation)
+	if err != nil {
+		fmt.Println("Error decoding JSON")
+		fmt.Println(err)
+		return
+	}
 
-    triangulation, err = Triangulate(triangulation.V)
-    if err != nil {
-        fmt.Println("Error computing triangulation:")
-        fmt.Println(err)
-        return
-    }
+	triangulation, err = Triangulate(triangulation.V)
+	if err != nil {
+		fmt.Println("Error computing triangulation:")
+		fmt.Println(err)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(triangulation)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(triangulation)
 }
 
-func setupRoutes() {
-    http.HandleFunc("/upload", uploadVertices)
-    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./triangulation"))))
-    http.ListenAndServe(":8030", nil)
+func setupRoutes(port string) {
+	http.HandleFunc("/upload", uploadVertices)
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./webpage"))))
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
 
 func main() {
-    fmt.Println("Go File Upload Tutorial")
-    setupRoutes()
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s <port>\n", os.Args[0])
+		return
+	}
+
+	setupRoutes(os.Args[1])
 }
--- triangulate.go
+++ triangulate.go
@@ -1,62 +1,62 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "os"
-    "os/exec"
-    "strings"
-    "strconv"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 type Vertices [][2]int
-type Edges    [][2]int
+type Edges [][2]int
 type Triangulation struct {
-    V Vertices
-    E Edges
+	V Vertices
+	E Edges
 }
 
 func Triangulate(vertices Vertices) (Triangulation, error) {
-    triangulation := Triangulation{}
+	triangulation := Triangulation{}
 
-    tempFile, err := ioutil.TempFile("tmp", "vertices_*.txt")
-    if err != nil {
-        return triangulation, err
-    }
-    defer tempFile.Close()
-    defer os.Remove(tempFile.Name())
+	tempFile, err := ioutil.TempFile("tmp", "vertices_*.txt")
+	if err != nil {
+		return triangulation, err
+	}
+	defer tempFile.Close()
+	defer os.Remove(tempFile.Name())
 
-    err = writeVertices(vertices, tempFile)
-    if err != nil {
-        return triangulation, err
-    }
+	err = writeVertices(vertices, tempFile)
+	if err != nil {
+		return triangulation, err
+	}
 
-    cmd := exec.Command("./delaunay", tempFile.Name())
-    out, err := cmd.Output()
-    if err != nil {
-        return triangulation, err
-    }
+	cmd := exec.Command("./triangulation/delaunay", tempFile.Name())
+	out, err := cmd.Output()
+	if err != nil {
+		return triangulation, err
+	}
 
-    triangulation.V = vertices
-    words := strings.Fields(string(out))
-    for i := 0; i < len(words); i += 2 {
-        a, _ := strconv.Atoi(words[i])
-        b, _ := strconv.Atoi(words[i+1])
-        triangulation.E = append(triangulation.E, [2]int{a, b})
-    }
-    return triangulation, nil
+	triangulation.V = vertices
+	words := strings.Fields(string(out))
+	for i := 0; i < len(words); i += 2 {
+		a, _ := strconv.Atoi(words[i])
+		b, _ := strconv.Atoi(words[i+1])
+		triangulation.E = append(triangulation.E, [2]int{a, b})
+	}
+	return triangulation, nil
 }
 
 func writeVertices(vertices Vertices, f *os.File) error {
-    _, err := f.WriteString(fmt.Sprintf("%d\n", len(vertices)))
-    if err != nil {
-        return err
-    }
-    for _, v := range vertices {
-        _, err = f.WriteString(fmt.Sprintf("%d %d\n", v[0], v[1]))
-        if err != nil {
-            return err
-        }
-    }
-    return nil
+	_, err := f.WriteString(fmt.Sprintf("%d\n", len(vertices)))
+	if err != nil {
+		return err
+	}
+	for _, v := range vertices {
+		_, err = f.WriteString(fmt.Sprintf("%d %d\n", v[0], v[1]))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
